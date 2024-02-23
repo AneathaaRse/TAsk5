26ffdc5f148a93a3d3f1993e268753cf039214ac
Aneathaa <159550441+AneathaaRse@users.noreply.github.com>
2024-02-23 12:29:07 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-// for loop
-
-const data = {
+{
     "name": "Aneathaa RSe",
     "age": 27
     "contact": {
@@ -10,7 +8,8 @@ const data = {
     },
     
 }
-        
+    
+      
     "education": [
       {
         "school": "Sree Sowdambika Convent matric.higher.sec.school"
@@ -22,73 +21,4 @@ const data = {
      "skills":Javascript,Python,Html
 
     ],
-   
-  for (let key in data) {
-    if (Array.isArray(data[key])) {
-      console.log(`${key}:`);
-      data[key].forEach(item => console.log(`- ${item}`));
-    } else if (typeof data[key] === 'object') {
-      console.log(`${key}:`);
-      for (let innerKey in data[key]) {
-        console.log(`- ${innerKey}: ${data[key][innerKey]}`);
-      }
-    } else {
-      console.log(`${key}: ${data[key]}`);
-    }
-  }
-
-  //forin loop
-  for (let key in data) {
-    if (Array.isArray(data[key])) {
-      console.log(`${key}:`);
-      data[key].forEach(item => console.log(`- ${item}`));
-    } else if (typeof data[key] === 'object') {
-      console.log(`${key}:`);
-      for (let innerKey in data[key]) {
-        console.log(`- ${innerKey}: ${data[key][innerKey]}`);
-      }
-    } else {
-      console.log(`${key}: ${data[key]}`);
-    }
-  }
-
-
-  //forEach loop
-
-  Object.keys(data).forEach(key => {
-    if (Array.isArray(data[key])) {
-      console.log(`${key}:`);
-      data[key].forEach(item => console.log(`- ${item}`));
-    } else if (typeof data[key] === 'object') {
-      console.log(`${key}:`);
-      Object.keys(data[key]).forEach(innerKey => {
-        console.log(`- ${innerKey}: ${data[key][innerKey]}`);
-      });
-    } else {
-      console.log(`${key}: ${data[key]}`);
-    }
-  });
-
-  //forof loop
-
-  for (let skill of data.skills) {
-    console.log(skill);
-  }
-  
-  // Using for...of with object keys
-  for (let key of Object.keys(data)) {
-    if (Array.isArray(data[key])) {
-      console.log(`${key}:`);
-      for (let item of data[key]) {
-        console.log(`- ${item}`);
-      }
-    } else if (typeof data[key] === 'object') {
-      console.log(`${key}:`);
-      for (let innerKey of Object.keys(data[key])) {
-        console.log(`- ${innerKey}: ${data[key][innerKey]}`);
-      }
-    } else {
-      console.log(`${key}: ${data[key]}`);
-    }
-  }
-
+    
